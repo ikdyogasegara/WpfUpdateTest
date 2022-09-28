013e45719da005d7289532c6acb3e0d0574e3099
I Kadek Yoga Segara <70006114+ikdyogasegara@users.noreply.github.com>
2022-09-29 01:34:39 +0800

    1.0.0

--- WPFCoreTest-main/WPFAppCore/MainWindow.xaml.cs
+++ WPFCoreTest-main/WPFAppCore/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace WPFAppCore
 {
     public partial class MainWindow : Window
     {
-        UpdateManager manager;
+        private UpdateManager _manager;
 
         public MainWindow()
         {
@@ -17,15 +17,20 @@ namespace WPFAppCore
 
         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            manager = await UpdateManager
-                .GitHubUpdateManager(@"https://github.com/meJevin/WPFCoreTest");
+            try
+            {
+                _manager = await UpdateManager
+                    .GitHubUpdateManager(@"https://github.com/ikdyogasegara/WpfUpdateTest");
 
-            CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+                CurrentVersionTextBox.Text = _manager.CurrentlyInstalledVersion().ToString();
+            }
+            catch{}
+            
         }
 
         private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
         {
-            var updateInfo = await manager.CheckForUpdate();
+            var updateInfo = await _manager.CheckForUpdate();
 
             if (updateInfo.ReleasesToApply.Count > 0)
             {
@@ -39,7 +44,7 @@ namespace WPFAppCore
 
         private async void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
-            await manager.UpdateApp();
+            await _manager.UpdateApp();
 
             MessageBox.Show("Updated succesfuly!");
         }
